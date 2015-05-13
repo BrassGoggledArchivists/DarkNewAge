7d16ef6d45303c86a7560142d2553a3801c96401
Arona Jones <warlordjones@gmail.com>
2015-05-13 15:24:50 +0100

    Whoops. Reinit repo with clean history

---
+++ src/main/java/darknewage/common/DarkNewAge.java
@@ -0,0 +1,49 @@
+package darknewage.common;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+import net.minecraftforge.common.config.Configuration;
+
+@Mod(modid = "darknewage", name = "Dark New Age", version = "Beta R1.0", dependencies = "after:BuildCraft|Core; after:TConstruct; after:ForgeMultipart; after:MineFactoryReloaded")
+public class DarkNewAge
+{
+
+	// @SidedProxy(clientSide = "boilerplate.client.ClientProxy", serverSide =
+	// "boilerplate.common.CommonProxy")
+	// public static CommonProxy proxy;
+
+	@Instance("darknewage")
+	public static DarkNewAge instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+
+		config.save();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+
+	}
+
+	@EventHandler
+	public void serverStarting(FMLServerStartingEvent event)
+	{
+	}
+}
